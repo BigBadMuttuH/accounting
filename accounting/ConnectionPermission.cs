a71ac9fe3f12cd0ea48b89acee9150e05caacb4d
Antonio Fogundes <bigbadantonio@gmail.com>
2023-04-15 05:17:47 +0300

    2023-04-15

--- accounting/ConnectionPermission.cs
+++ accounting/ConnectionPermission.cs
@@ -3,8 +3,6 @@
     public class ConnectionPermission
     {
         public int Id { get; set; }         // ID в базе данных
-        public int DeviceId { get; set; }   // ID устройства в базе данных
-        public int UserId { get; set; }     // ID Пользователя в базе дынных
         public string PermissionNumber { get; set; } // Номер предписания 
         public DateTime PermissionDate { get; set; } // Дата регистрации предписания
         public string RegistrationNumber { get; set; } // Регистрационный номер в системе ДО 
@@ -12,10 +10,8 @@
 
         public ConnectionPermission() { }
 
-        public ConnectionPermission(int deviceId, int userId, string permissionNumber, DateTime permissionDate, string registrationNumber, string url)
+        public ConnectionPermission(string permissionNumber, DateTime permissionDate, string registrationNumber, string url)
         {
-            DeviceId = deviceId;
-            UserId = userId;
             PermissionNumber = permissionNumber;
             PermissionDate = permissionDate;
             RegistrationNumber = registrationNumber;
