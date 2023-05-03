a5b5077e231f8ef49dc9ce03e96c101067749032
BigBadMuttuh <bigbadantonio@gmail.com>
2023-05-03 14:29:58 +0300

    2023-05-03-7

--- accounting/Program.cs
+++ accounting/Program.cs
@@ -5,10 +5,10 @@ using accounting.View;
 Console.WriteLine("Initializing...");
 while (true)
 {
-    var deviceView = new DeviceView();
-    var deviceDataAccess = new DeviceDataAccess();
-    var deviceController = new DeviceController(deviceDataAccess, deviceView);
-    deviceController.Start();
+    //var deviceView = new DeviceView();
+    //var deviceDataAccess = new DeviceDataAccess();
+    //var deviceController = new DeviceController(deviceDataAccess, deviceView);
+    //deviceController.Start();
 
     //var connectionPermissionView = new ConnectionPermissionView();
     //var connectionPermissionDataAccess = new ConnectionPermissionDataAccess();
@@ -16,13 +16,13 @@ while (true)
         //new ConnectionPermissionController(connectionPermissionDataAccess, connectionPermissionView);
     //connectionPermissionController.Start();
 
-    // var userView = new UserView();
-    // var userDataAccess = new UserDataAccess();
-    // var userController = new UserController(userDataAccess, userView);
-    // userController.Start();
+     //var userView = new UserView();
+     //var userDataAccess = new UserDataAccess();
+     //var userController = new UserController(userDataAccess, userView);
+     //userController.Start();
 
-    //var accountingView = new AccountingView();
-    //var accountingDataAccess = new AccountingDataAccess();
-    //var accountingController = new AccountingController(accountingDataAccess, accountingView);
-    //accountingController.Start();
+    var accountingView = new AccountingView();
+    var accountingDataAccess = new AccountingDataAccess();
+    var accountingController = new AccountingController(accountingDataAccess, accountingView);
+    accountingController.Start();
 }
